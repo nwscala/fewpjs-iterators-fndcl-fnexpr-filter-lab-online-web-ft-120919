3e19666d5a0a3b6402b5f141e96db1101b21d829
Noah Scala <nwscala@gmail.com>
2020-04-03 14:55:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driversArray, string) {
+    return driversArray.filter(driverName => driverName.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(driversArray, string) {
+    const myReg = new RegExp(`^${string}`, 'i')
+    return driversArray.filter(driverName => myReg.test(driverName))
+}
+
+function matchName(driversObjectArray, string) {
+    return driversObjectArray.filter(driversObject => driversObject.name === string)
+}
